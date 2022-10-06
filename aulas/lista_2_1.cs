bf99a2ba093352fefe65270eca9aa04c0618f140
JOSE ELTON <115085872+eltonsilva23@users.noreply.github.com>
2022-10-06 12:45:16 +0000

    alteração

--- aulas/lista_2_1.cs
+++ aulas/lista_2_1.cs
@@ -5,7 +5,7 @@ public class Program {
   public static void Main(string[] args) {
     Console.Write("Digite seu primeiro nome\n");
     string s =Console.ReadLine();
-    s = s.remove(s.IndexOf(' '))
+    s = s.Remove(s.IndexOf(' '));
     Console.WriteLine("Bem-vindo ao C#, " + s);
   }
 }
