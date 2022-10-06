78660a9caa8527a88e276c62ad19cce855642441
eltonsilva23 <115085872+eltonsilva23@users.noreply.github.com>
2022-10-06 10:00:35 -0300

    Delete lista_1.cs

--- aulas/lista_1.cs
+++
@@ -1,9 +0,0 @@
-using System; //teste de push
-
-public class Program {
-
-  public static void Main(string[] args) {
-    Console.WriteLine("Jose Elton de Araujo Silva");
-    Console.WriteLine("20221014040012");
-  }
-}
